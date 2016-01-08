7ea291263db30f019bc90c88077666aac6ee85fe
Paul Chubinskiy <pchubinskiy@gmail.com>
2016-01-08 13:15:38 -0600

    wrote basic error handling

--- app.rb
+++ app.rb
@@ -18,9 +18,20 @@ get '/add' do
 end
 
 post '/add' do
+
   growl = params[:user_growl]
+  name = params[:user_name]
+
+  if growl.length > 142
+    @error = true
+    @error_msg = "Nope"
+    @name_before_error = name
+    @growl_before_error = growl
+    erb :add
+  else
   session[:growls].push growl
 
   redirect '/'
+  end
 end
 
