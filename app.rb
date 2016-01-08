372757ab4ae10d488f0de7ac44edb8219f240335
Metel Patel <metelpatel@gmail.com>
2016-01-08 11:21:38 -0600

    established add path, working on sessions

--- app.rb
+++ app.rb
@@ -1,12 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-before do
+enable :sessions
 
+before do
+  session[:growls] ||= []
 end
 
 # Routes
-get '/' do 
-
+get '/' do
+  @growls = session[:growls]
 	erb :index
 end
+
+get '/add' do
+  erb :add
+end
+
+post '/add' do
+  growl = params[:user_growl]
+  session[:growls].push growl
+
+  redirect '/'
+end
+
