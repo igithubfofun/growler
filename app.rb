ab13b31d89c8a3f721924a4610dbb8582c8b1f57
Metel Patel <metelpatel@gmail.com>
2016-01-08 14:48:20 -0600

    got list to work properly

--- app.rb
+++ app.rb
@@ -5,11 +5,13 @@ enable :sessions
 
 before do
   session[:growls] ||= []
+  session[:names] ||= []
 end
 
 # Routes
 get '/' do
   @all_growls = session[:growls]
+  @all_names = session[:names]
 	erb :index
 end
 
@@ -21,17 +23,29 @@ post '/add' do
 
   growl = params[:user_growl]
   name = params[:user_name]
+  growlset = {0 => name, 1 => growl}
 
   if growl.length > 142
     @error = true
-    @error_msg = "Nope"
+    @error_msg = "Too many characters! Max: 142"
     @name_before_error = name
     @growl_before_error = growl
     erb :add
   else
-  session[:growls].push growl
+
+  session[:growls].push growlset
+
 
   redirect '/'
   end
+end
+
+get '/logout' do
+
+    session.clear
+    session[:@all_growls] = []
+    session[:@all_names] = []
+    erb :logout
+
 end
 
